e9600e4dd8317846056c4f2aee83da6a25d59320
Pablo Erlis <pabloerlis@gmail.com>
2022-04-05 18:01:33 -0300

    Upload do teste

---
+++ TestePoo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestePoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Desafio4();
+        }
+
+        private static void Desafio4()
+        {
+            string[] vogais = { "a", "e", "i", "o", "u" };
+
+            Console.WriteLine("Digite uma letra: ");
+            string letra = Console.ReadLine().ToLower();
+            bool ehVogal = false;
+
+            foreach (var item in vogais)
+            {
+                if (letra == item)
+                {
+                    ehVogal = true;
+
+                }
+            }
+
+            if (ehVogal)
+            {
+                Console.WriteLine($"A letra {letra} é uma vogal!");
+            }
+            else
+            {
+                Console.WriteLine($"A letra {letra} é uma consoante!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+    
+}
